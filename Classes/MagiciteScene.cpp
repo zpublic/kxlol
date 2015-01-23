4bf7816bbf1a3fba6ea92fab76849f89dfaa18e6
zapline <278998871@qq.com>
2015-01-23 23:25:55 +0800

    * kxlol.vcxproj.user

---
+++ Classes/MagiciteScene.cpp
@@ -0,0 +1,14 @@
+#include "MagiciteScene.h"
+
+USING_NS_CC;
+
+bool MagiciteScene::init()
+{
+    if ( !Scene::init() )
+    {
+        return false;
+    }
+
+
+    return true;
+}
---
+++ Classes/MagiciteScene.h
@@ -0,0 +1,14 @@
+#ifndef __MAGICITE_SCENE_H__
+#define __MAGICITE_SCENE_H__
+
+#include "cocos2d.h"
+
+class MagiciteScene : public cocos2d::Scene
+{
+public:
+    virtual bool init();
+
+    CREATE_FUNC(MagiciteScene);
+};
+
+#endif // __MAGICITE_SCENE_H__
