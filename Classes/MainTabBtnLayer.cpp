e6429c770a10439247262dcc837e10b44d40beca
CoderNeko <maodehuanxiangji@163.com>
2015-01-24 19:35:28 +0800

    添加主场景Tab切换

--- Classes/MainTabBtnLayer.cpp
+++ Classes/MainTabBtnLayer.cpp
@@ -1,9 +1,16 @@
 #include "MainTabBtnLayer.h"
+#include "MainScene.h"
+#include "MainOperatePanel.h"
+#include "MainOperateInfoPanel.h"
 
 USING_NS_CC;
 
 bool MainTabBtnLayer::init()
 {
+	pCurSelBtn = nullptr;
+	pOpre = nullptr;
+	pOpreInfo = nullptr;
+
     if ( !Layer::init() )
     {
         return false;
@@ -13,9 +20,9 @@ bool MainTabBtnLayer::init()
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
     auto closeItem = MenuItemImage::create(
-        "CloseNormal.png",
         "CloseSelected.png",
-        CC_CALLBACK_1(MainTabBtnLayer::TabBtnCallback, this));
+        "CloseSelected.png",
+		CC_CALLBACK_1(MainTabBtnLayer::MainGodSpaceCallBack, this));
     closeItem->setPosition(
         Vec2(
         origin.x + 100,
@@ -24,11 +31,12 @@ bool MainTabBtnLayer::init()
     auto menu = Menu::create(closeItem, NULL);
     menu->setPosition(Vec2::ZERO);
     this->addChild(menu, 1);
+	pCurSelBtn = closeItem;
 
     closeItem = MenuItemImage::create(
         "CloseNormal.png",
         "CloseSelected.png",
-        CC_CALLBACK_1(MainTabBtnLayer::TabBtnCallback, this));
+		CC_CALLBACK_1(MainTabBtnLayer::PlanesMgrCenter, this));
     closeItem->setPosition(
         Vec2(
         origin.x + 300,
@@ -41,7 +49,7 @@ bool MainTabBtnLayer::init()
     closeItem = MenuItemImage::create(
         "CloseNormal.png",
         "CloseSelected.png",
-        CC_CALLBACK_1(MainTabBtnLayer::TabBtnCallback, this));
+		CC_CALLBACK_1(MainTabBtnLayer::OriginUniverse, this));
     closeItem->setPosition(
         Vec2(
         origin.x + 500,
@@ -54,7 +62,7 @@ bool MainTabBtnLayer::init()
     closeItem = MenuItemImage::create(
         "CloseNormal.png",
         "CloseSelected.png",
-        CC_CALLBACK_1(MainTabBtnLayer::TabBtnCallback, this));
+		CC_CALLBACK_1(MainTabBtnLayer::PowerCapsule, this));
     closeItem->setPosition(
         Vec2(
         origin.x + 700,
@@ -67,7 +75,50 @@ bool MainTabBtnLayer::init()
     return true;
 }
 
-void MainTabBtnLayer::TabBtnCallback(Ref* pSender)
+void MainTabBtnLayer::TabChange(Ref* pSender)
 {
-    ;
+	if(nullptr != pCurSelBtn)
+	{
+		auto pCurBtn = dynamic_cast<MenuItemImage*>(pCurSelBtn);
+		if(nullptr != pCurBtn)
+			pCurBtn->setNormalImage(Sprite::create("CloseNormal.png"));
+	}
+	pCurSelBtn = pSender;
+	auto pSenderBtn = dynamic_cast<MenuItemImage*>(pSender);
+	if(nullptr != pSenderBtn)
+		pSenderBtn->setNormalImage(Sprite::create("CloseSelected.png"));
+
+	pOpre->removeAllChildren();
+	pOpreInfo->removeAllChildren();
+
+}
+
+void MainTabBtnLayer::MainGodSpaceCallBack(Ref* pSender)
+{
+	TabChange(pSender);
+	pOpre->addChild(MainOperatePanel::create());
+    pOpreInfo->addChild(MainOperateInfoPanel::create());
+}
+
+void MainTabBtnLayer::PlanesMgrCenter(Ref* pSender)
+{
+	TabChange(pSender);
+}
+
+void MainTabBtnLayer::OriginUniverse(Ref* pSender)
+{
+	TabChange(pSender);
+}
+
+void MainTabBtnLayer::PowerCapsule(Ref* pSender)
+{
+	TabChange(pSender);
+}
+
+void MainTabBtnLayer::setOperatorLayer(Node* pOpre, Node* pOpreInfo)
+{
+	this->pOpre = pOpre;
+	this->pOpreInfo = pOpreInfo;
+	pOpre->addChild(MainOperatePanel::create());
+    pOpreInfo->addChild(MainOperateInfoPanel::create());
 }
