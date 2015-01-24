db1cd0885140f5f3e723d3dfcdea3127d516ec4d
CoderNeko <maodehuanxiangji@163.com>
2015-01-25 02:12:35 +0800

    绘制主场景界面

--- Classes/MainBgLayer.cpp
+++ Classes/MainBgLayer.cpp
@@ -12,9 +12,9 @@ bool MainBgLayer::init()
     Size visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
 
-//     auto sprite = Sprite::create("HelloWorld.png");
-//     sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
-//     this->addChild(sprite, 0);
+     auto sprite = Sprite::create("img/MainScene/bg.png");
+     sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+     this->addChild(sprite, 0);
 
     return true;
 }
