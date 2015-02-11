ead1271c029e9f66728d0482db07fab589448f87
CoderNeko <me@coderneko.com>
2015-02-11 23:00:57 +0800

    添加开场白点击屏幕快速结束

--- Classes/MagiciteScene.cpp
+++ Classes/MagiciteScene.cpp
@@ -23,15 +23,36 @@ bool MagiciteScene::init()
     this->addChild(lblStory);
     lblStory->setOpacity(0);
 
-    Vector<FiniteTimeAction*> actions;
-    actions.pushBack(FadeIn::create(0.3f));
-    actions.pushBack(DelayTime::create(str.length() * 0.01f));
-    actions.pushBack(FadeOut::create(0.2f));
-    actions.pushBack(CallFunc::create([&](){
+    //添加一个透明的按钮与视图等大的按钮来响应点击屏幕的消息
+    auto btnBg = ui::Button::create("img/_blank.png");
+    this->addChild(btnBg);
+    btnBg->setPosition(Vec2(origin.x + visibleSize.width / 2, origin.y + visibleSize.height / 2));
+    btnBg->setZoomScale(1.0f);
+    btnBg->setScale(visibleSize.width / btnBg->getContentSize().width, 
+        visibleSize.height / btnBg->getContentSize().height);
+    
+    Vector<FiniteTimeAction*> actionIn;
+    Vector<FiniteTimeAction*> actionOut;
+    actionIn.pushBack(FadeIn::create(0.3f));
+    actionIn.pushBack(DelayTime::create(str.length() * 0.01f));
+    actionOut.pushBack(CallFunc::create([btnBg](){
+        btnBg->setEnabled(false);
+    }));
+    actionOut.pushBack(FadeOut::create(0.2f));
+    actionOut.pushBack(CallFunc::create([this, btnBg](){
+        btnBg->runAction(RemoveSelf::create());
         this->addChild(MagiciteGameLayer::create());
     }));
-    actions.pushBack(RemoveSelf::create(true));
-    lblStory->runAction(Sequence::create(actions));
+    actionOut.pushBack(RemoveSelf::create(true));
+    actionIn.pushBack(Sequence::create(actionOut));
+    lblStory->runAction(Sequence::create(actionIn));
+
+    btnBg->addTouchEventListener([lblStory, actionOut](Ref*, ui::Widget::TouchEventType eve){
+        if(eve == ui::Widget::TouchEventType::BEGAN){
+            lblStory->stopAllActions();
+            lblStory->runAction(Sequence::create(actionOut));
+        }
+    });
     
     return true;    
 }
