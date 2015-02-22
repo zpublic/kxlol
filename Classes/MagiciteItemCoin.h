2e1b4bf4f826232c51f0791f510a3017b2fda000
kingwl <805037171@163.com>
2015-02-22 22:57:14 +0800

    添加 硬币物品

---
+++ Classes/MagiciteItemCoin.h
@@ -0,0 +1,24 @@
+#ifndef __MAGICITE_ITEM_COIN__
+#define __MAGICITE_ITEM_COIN__
+
+#include "cocos2d.h"
+#include "MagiciteTempItem.h"
+#include "MagiciteItemCoinObject.h"
+
+class MagiciteItemCoin : public MagiciteTempItem
+{
+public:
+    enum : int
+    {
+        life = 1,
+    };
+
+    static std::string name;
+
+    MagiciteItemCoin();
+    ~MagiciteItemCoin();
+
+    virtual MagiciteItemObject* getItemObject();
+};
+
+#endif //__MAGICITE_ITEM_COIN__
